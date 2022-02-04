d7f258b03ccfc4ab06c63cb16f74bc7d75b8eb49
Vander <vandercomp@gmail.com>
2022-02-04 15:12:57 -0300

    deploy

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -2,6 +2,7 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
+/*
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 
@@ -72,4 +73,5 @@ const checkPageComponent = async (page) => {
   });
 };
 
-// pages.forEach((page) => checkPageComponent(page));
+pages.forEach((page) => checkPageComponent(page));
+*/
